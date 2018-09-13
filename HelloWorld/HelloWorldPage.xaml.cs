128225b145326619dbf74f2a1cc98db5da9e6f86
Zachary.Hirn@gmail.com <Zachary.Hirn@gmail.com>
2018-09-12 20:04:53 -0500

    Added my stuff

---
+++ HelloWorld/HelloWorldPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+using System;
+
+namespace HelloWorld
+{
+    public partial class HelloWorldPage : ContentPage
+    {
+        public HelloWorldPage()
+        {
+            InitializeComponent();
+        }
+
+        string message1 = "Hello World!";
+        string message2 = "Goodbye World!";
+
+        void HandleButtonClick(object sender, System.EventArgs e)
+        {
+            if(MyAwesomeLabel.Text == message1)
+            {
+                MyAwesomeLabel.Text = message2;
+            }
+            else
+            {
+                MyAwesomeLabel.Text = message1;
+            }
+        }
+    }
+}
